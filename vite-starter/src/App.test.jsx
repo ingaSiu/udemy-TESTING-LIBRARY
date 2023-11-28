563c96bc4b6042325228c4346a34bc76c3567174
ingaSiu <ingasiud@gmail.com>
2023-11-28 19:55:37 +0200

    analyzed written test

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
 
-test("App contains correct heading", () => {
+import App from './App';
+
+// render method:
+// creates simulated DOM for argument component
+// once rendered simulated DOM can be accessed via global object called screen
+// getByText - is a method from screen (it is looking at simulated DOM and trying to find an element
+// what matches the text that we give it)
+
+test('App contains correct heading', () => {
   render(<App />);
   const headingElement = screen.getByText(/learn react/i);
+
+  // assertion which determines if test pass or fail
+  // (linkElement) - expect argument(subject of the assertion)
+  // .toBeInTheDocument() - matcher (type of assertion), it can have argument which refines matcher
   expect(headingElement).toBeInTheDocument();
 });
+
+// /learn react/i - this is a regular expression (i means case insensitive )
+// with regulat expression it can be partial match
+// but if string is used it has to be an exact match
